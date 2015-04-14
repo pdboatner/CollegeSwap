05ccdc4d42e8746fac142da419ded01882ba59b9
Patrick Boatner <patrickboatner@gmail.com>
2015-04-14 16:47:02 -0500

    Slight refactoring in database package (missed one)

--- Android/CollegeSwap/app/src/main/java/edu/ua/collegeswap/database/TextbookWriter.java
+++ Android/CollegeSwap/app/src/main/java/edu/ua/collegeswap/database/TextbookWriter.java
@@ -1,6 +1,7 @@
 package edu.ua.collegeswap.database;
 
 import edu.ua.collegeswap.viewModel.Listing;
+import edu.ua.collegeswap.viewModel.Textbook;
 
 /**
  * Created by Patrick on 4/9/2015.
@@ -14,6 +15,16 @@ public class TextbookWriter extends ListingWriter {
     @Override
     public void updateExisting(Listing updatedListing) {
         //TODO
+
+        Textbook textbook = (Textbook) updatedListing;
+
+        // TODO Get the information from the textbook and send it to the server
+        textbook.getTitle();
+        textbook.getAskingPrice();
+        textbook.getCourseNumber();
+        textbook.getCourseSubject();
+
+
     }
 
     @Override
