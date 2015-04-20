b1b340c36c3acf3db171b2f3283868bdb71c0c78
Kerber <jpkerber92@gmail.com>
2015-04-20 12:26:22 -0500

    Upgraded edit profile to match others
    had no crimson..needed more

--- Android/CollegeSwap/app/src/main/java/edu/ua/collegeswap/view/EditProfileActivity.java
+++ Android/CollegeSwap/app/src/main/java/edu/ua/collegeswap/view/EditProfileActivity.java
@@ -1,36 +1,45 @@
 package edu.ua.collegeswap.view;
 
 import android.content.SharedPreferences;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import edu.ua.collegeswap.R;
+import edu.ua.collegeswap.viewModel.Account;
 
 /* The EditProfileActivity class allows the user to edit their profile.
  * This activity is launched via the FragmentProfile.
  */
 
-public class EditProfileActivity extends ActionBarActivity {
+public class EditProfileActivity extends ActionBarActivity implements View.OnClickListener {
 
     private static final String PREFS_NAME = "MyPreferences";
+
+    private Account account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        setupActionBar();
 
 
-        EditText phoneNum = (EditText) findViewById(R.id.editTextPhoneNumber);
-        EditText email = (EditText) findViewById(R.id.editTextEmailAddress);
+        EditText phoneNum = (EditText) findViewById(R.id.editTextPhone);
+        EditText email = (EditText) findViewById(R.id.editTextEmail);
 
-        phoneNum.setText("256-123-4567");
-        email.setText("soccerMom203@crimson.ua.edu");
+
+        phoneNum.setHint("256-123-4567");
+        email.setHint("soccerMom@crimson.ua.edu");
 
 
     }
@@ -39,7 +48,7 @@ public class EditProfileActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_profile, menu);
+        //getMenuInflater().inflate(R.menu.menu_edit_profile, menu);
         return true;
     }
 
@@ -49,44 +58,74 @@ public class EditProfileActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
 
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            return true;
-        } else if (id == R.id.action_save) {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-            String username = prefs.getString(getString(R.string.pref_username_key), getString(R.string.pref_username_default));
-            saveButtonClicked();
-
-        }
-        // Respond to the action bar's Up/Home button
-        else if (id == android.R.id.home) {
-            /*
-            This prevents the MainDrawerActivity from reverting to the default Section number.
-            This makes the Up/Home button behavior identical to the hardware Back button behavior.
-            */
-            finish();
-            return true;
-        }
-
-//        switch(item.getItemId()) {
-//            case (R.id.action_save) :
-//                saveButtonClicked();
-//                break;
-//            case (R.id.action_settings) :
-//                break;
+//      else if (id == R.id.action_save) {
+//            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//            String username = prefs.getString(getString(R.string.pref_username_key), getString(R.string.pref_username_default));
+//            saveButtonClicked();
+//
 //        }
 
-        return super.onOptionsItemSelected(item);
+
+        return true;
     }
 
 
     private void saveButtonClicked() {
           Toast.makeText(this, "Save button clicked", Toast.LENGTH_SHORT).show();
-          SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
-          SharedPreferences.Editor editor = sharedPreferences.edit();
-          editor.putString(getString(R.string.pref_username_key), "newUser");
+//          SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+//          SharedPreferences.Editor editor = sharedPreferences.edit();
+//          editor.putString(getString(R.string.pref_username_key), "newUser");
+    }
+
+    /**
+     * See https://plus.google.com/+RomanNurik/posts/R49wVvcDoEW and https://android.googlesource.com/platform/developers/samples/android/+/master/ui/actionbar/DoneBar
+     */
+    protected void setupActionBar() {
+        final LayoutInflater inflater = (LayoutInflater) getSupportActionBar()
+                .getThemedContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View customActionBarView = inflater.inflate(R.layout.actionbar_done_cancel, null);
+        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(this);
+        customActionBarView.findViewById(R.id.actionbar_cancel).setOnClickListener(this);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM, ActionBar.DISPLAY_SHOW_CUSTOM
+                | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(customActionBarView,
+                new ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.actionbar_cancel:
+                finish();
+                break;
+            case R.id.actionbar_done:
+                // Save the Ticket. Submit it to the server.
+
+//                ticket.setTitle(title.getText().toString());
+//                ticket.setAskingPrice(Float.parseFloat(price.getText().toString()));
+//                ticket.setDetails(details.getText().toString());
+//                ticket.setBowl(bowl.getSelectedItem().toString());
+//                ticket.setGame(game.getSelectedItem().toString());
+//                //TODO check the indices of the spinners. Should not be 0 - the hint.
+//
+//                TicketWriter ticketWriter = new TicketWriter();
+//                if (editingExisting) {
+//                    ticketWriter.updateExisting(ticket);
+//                    Toast.makeText(this, "Updating existing ticket", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    ticketWriter.saveNew(ticket);
+//                    Toast.makeText(this, "Saving new ticket", Toast.LENGTH_SHORT).show();
+//                }
+
+                saveButtonClicked();
+                finish(); // TODO ask the calling activity to refresh now?
+
+                break;
+        }
+
     }
 }
--- Android/CollegeSwap/app/src/main/java/edu/ua/collegeswap/view/LoginActivity.java
+++ Android/CollegeSwap/app/src/main/java/edu/ua/collegeswap/view/LoginActivity.java
@@ -51,7 +51,7 @@ public class LoginActivity extends Activity {
 
         //TODO check the users login credentials against the server
 
-        return true; // too much work to type things
+        return true; // too much work to type things  <-- lazy! XD
 
 //        if (username.equals("bob") && password.equals("1234")) {
 //            return true;
@@ -70,25 +70,4 @@ public class LoginActivity extends Activity {
         startActivity(i);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_login, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
